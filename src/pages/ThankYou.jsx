676d5c707d7cfc119bd72c58df47575ed0dce683
RR <rrn091301@gmail.com>
2025-12-18 14:59:57 +0800

    feat: add thank you page content

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,3 +1,12 @@
 export default function ThankYou() {
-	return <div>Thank You</div>;
+	return (
+		<div>
+			<h1>Thank You</h1>
+			<p>
+				Thanks for confirming your subscription! We hope you have fun using our
+				platform. If you ever need support, please feel free to email us at
+				support@loremgaming.com.
+			</p>
+		</div>
+	);
 }
